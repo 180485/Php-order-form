89289ca987d4b4965dbafd729b2f66f90aa387fa
180485 <ismiandhykawaty@icloud.com>
2021-01-07 14:18:10 +0100

    make input data

--- form-view.php
+++ form-view.php
@@ -9,29 +9,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
           rel="stylesheet"/>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
     <title>Your fancy store</title>
 </head>
 <body>
-<div class="container">
-    <h1>Place your order</h1>
-    <?php // Navigation for when you need it ?>
-    <?php /*
-    <nav>
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="?food=1">Order food</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?food=0">Order drinks</a>
-            </li>
-        </ul>
-    </nav>
-    */ ?>
+    <div class="container jumbotron">
+        <h1 class="text-center text-muted "  >Order food in restaurant <i class="fas fa-glass-cheers"></i> <br /> "The YOLO - Cocktail & Sushi Bar"</h1>
+        <nav class="mb-3 mt-3 ">
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link active bg-info text-white" href="?food=1">Order Food</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-info" href="?food=0">Order Drunk</a>
+                </li>
+            </ul>
+        </nav>
     <form method="post">
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
                 <input type="text" id="email" name="email" class="form-control"/>
+                <?php echo "<p class='text-danger'>$errEmail</p>";?>
             </div>
             <div></div>
         </div>
@@ -43,20 +42,24 @@
                 <div class="form-group col-md-6">
                     <label for="street">Street:</label>
                     <input type="text" name="street" id="street" class="form-control">
+                    <?php echo "<p class='text-danger'>$errStreet</p>";?>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetnumber">Street number:</label>
                     <input type="text" id="streetnumber" name="streetnumber" class="form-control">
+                    <?php echo "<p class='text-danger'>$errSnumber</p>";?>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City:</label>
                     <input type="text" id="city" name="city" class="form-control">
+                    <?php echo "<p class='text-danger'>$errCity</p>";?>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="zipcode">Zipcode</label>
                     <input type="text" id="zipcode" name="zipcode" class="form-control">
+                    <?php echo "<p class='text-danger'>$errZipcode</p>";?>
                 </div>
             </div>
         </fieldset>
@@ -71,12 +74,22 @@
             <?php endforeach; ?>
         </fieldset>
 
-        <button type="submit" class="btn btn-primary">Order!</button>
+        <button type="submit" class="btn btn-info">Order!</button>
     </form>
-
     <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> in food and drinks.</footer>
+    <?php
+    echo "<h3>Your Input :</h3>";
+    echo $email;
+    echo "<br>";
+    echo $street;
+    echo "<br>";
+    echo $streetnumber;
+    echo "<br>";
+    echo $zipcode;
+    echo "<br>";
+    echo $products;
+   ?>
 </div>
-
 <style>
     footer {
         text-align: center;
--- index.php
+++ index.php
@@ -18,20 +18,71 @@ function whatIsHappening() {
     echo '<h2>$_SESSION</h2>';
     var_dump($_SESSION);
 }
+// define variables and set to empty values
+$emailErr = $streetErr = $streetnumberErr = $zipcodeErr = $productsErr ="";
+$email= $street = $streetnumber = $zipcode = $products = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["email"])) {
+        $email = "Missing";
+    }
+    else {
+        $email = $_POST["email"];
+    }
+
+    if (empty($_POST["street"])) {
+        $street = "Missing";
+    }
+    else {
+        $street = $_POST["street"];
+    }
+
+    if (empty($_POST["streetnumber"]))  {
+        $streetnumber = "Missing";
+    }
+    else {
+        $streetnumber = $_POST["streetnumber"];
+    }
+
+    if (!isset($_POST["zipcode"])) {
+        $zipcode = "Only Number";
+    }
+    else {
+        $zipcode = $_POST["zipcode"];
+    }
+
+}
+
+
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  
+
 
 // TODO: provide some products (you may overwrite the example)
 
 $products = [
+    ['name' => 'Whiskey Sour', 'price' => 10],
+    ['name' => 'Bloody Caesar', 'price' =>9.5],
+    ['name' => 'Margarita', 'price' => 9],
+    ['name' => 'Sake', 'price' => 8],
     ['name' => 'Ramen From the chef', 'price' => 10.20],
     ['name' => 'Sashimi Salmon', 'price' => 8],
     ['name' => 'California Roll', 'price' => 9],
     ['name' => 'Scallop Roll', 'price' => 10],
     ['name' => 'Spicy Tuna', 'price' => 6],
-    ['name' => 'Whiskey Sour', 'price' => 10],
-    ['name' => 'Bloody Caesar', 'price' =>9.5],
-    ['name' => 'Margarita', 'price' => 9],
-    ['name' => 'Sake', 'price' => 8],
+   
 ];
+
+
+
+
 
 $totalValue = 0;
 
