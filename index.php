bdc504fc893a5bbdfc0173cd8f2356a67c34fe8a
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-03-01 16:31:46 +0100

    adding gif

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-/*function whatIsHappening//() {
-    echo '<h2>$_GET</h2>';
-    var_dump($_GET);
-    echo '<h2>$_POST</h2>';
-    var_dump($_POST);
-    echo '<h2>$_COOKIE</h2>';
-    var_dump($_COOKIE);
-    echo '<h2>$_SESSION</h2>';
-    var_dump($_SESSION);
-
-
-}*/
-
-//whatIsHappening(); 
 session_start();
 
 $emailErr = $streetErr = $numberErr = $cityErr = $zipcodeErr = $productErr = "";
